872f4a868ccd529f66ec652246cf7adf4b38ff8b
Edgar Barrera <80984699+EdgarCastillo101@users.noreply.github.com>
2021-05-21 12:15:24 -0500

    Update Lineartime.py

--- Lineartime.py
+++ Lineartime.py
@@ -1,3 +1,6 @@
+# Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
+# Copyright (c) 2021 Edgar Barrera
+
 array = [1, 2, 3, 4, 5]
 for element in array:
     print(element)
