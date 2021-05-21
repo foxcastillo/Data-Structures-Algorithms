bb8da6bf86616665b33845a880723ca7606496f3
Edgar Barrera <80984699+EdgarCastillo101@users.noreply.github.com>
2021-05-21 12:13:29 -0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,6 @@
+# Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
+# Copyright (c) 2021 Edgar Barrera
+
 def fibonacci(n):
     assert n >= 0 and int(n) == n, 'The number must be positive integer only!'
     if n in [0,1]:
