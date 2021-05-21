b97439a77aa095b266b5f5d0438a7ac7546b7664
Edgar Barrera <80984699+EdgarCastillo101@users.noreply.github.com>
2021-05-21 12:17:46 -0500

    Update RecursionEX.py

--- RecursionEX.py
+++ RecursionEX.py
@@ -1,4 +1,5 @@
-
+# Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
+# Copyright (c) 2021 Edgar Barrera
 
 def factorial(n):
     assert n >=0 and int(n) == n, 'The number must be positive integer only!'
