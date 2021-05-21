b6d476c37b0284dd392284ece2c1b5a37a98309c
Edgar Barrera <80984699+EdgarCastillo101@users.noreply.github.com>
2021-05-21 12:16:30 -0500

    Update ListOperations.py

--- ListOperations.py
+++ ListOperations.py
@@ -1,3 +1,6 @@
+# Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
+# Copyright (c) 2021 Edgar Barrera
+
 # List operations / functions
 print("first one: ")
 a = [1,2,3]
