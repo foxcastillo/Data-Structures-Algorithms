690da202b4c0e9d31fb19f44add70e9496783bf9
Edgar Barrera <80984699+hellocastillo@users.noreply.github.com>
2021-06-07 15:47:37 -0500

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,5 +1,5 @@
-# Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
-# Copyright (c) 2021 Edgar Barrera
+# Edgar Castillo / Github: https://github.com/hellocastillo
+# Copyright (c) 2021 Edgar Castillo
 
 # Linked List is a form of a sequential collection and it does not have to be in order.
 # A Linked List is made up of independent nodes that may contain any type of data and each node has a reference to the next node in the link.
