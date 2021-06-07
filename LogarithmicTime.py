e735067ec1c5b3334572c3b6261e9f66730ef827
Edgar Barrera <80984699+hellocastillo@users.noreply.github.com>
2021-06-07 15:44:32 -0500

    Update LogarithmicTime.py

--- LogarithmicTime.py
+++ LogarithmicTime.py
@@ -1,5 +1,5 @@
-# Edgar Barrera / Github: https://github.com/hellocastillo
-# Copyright (c) 2021 Edgar Barrera
+# Edgar Castillo / Github: https://github.com/hellocastillo
+# Copyright (c) 2021 Edgar Castillo
 
 array = [1, 2, 3, 4, 5]
 for index in range(0, len(array), 3):
